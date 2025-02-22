529d3177fa4f7432f9baa6139c3ccf8f2de488f9
Kevin Chen <kchen@lcs.on.ca>
2025-02-22 11:08:56 -0500

    Initial Commit

---
+++ Addition/AdditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionApp.swift
+//  Addition
+//
+//  Created by Haowen Chen on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct AdditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
